6b53c3c1c30a6d5512b35485c495c6a7ec006c22
Maksym <pozetiv13@gmail.com>
2026-04-30 12:55:26 +0300

    Maksym Gorodnychyi

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -4,36 +4,43 @@ import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int MAX_SIZE = 10;
-    private K[] keys;
-    private V[] values;
+    private Entry<K, V>[] entries;
     private int size;
+
+    private static class Entry<K, V> {
+        private K key;
+        private V value;
+
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     @SuppressWarnings("unchecked")
     public StorageImpl() {
-        keys = (K[]) new Object[MAX_SIZE];
-        values = (V[]) new Object[MAX_SIZE];
+        entries = new Entry[MAX_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < size; i++) {
-            boolean keysAreEqual = keys[i] == null ? key == null : keys[i].equals(key);
+            boolean keysAreEqual = entries[i].key == null ? key == null : entries[i].key.equals(key);
             if (keysAreEqual) {
-                values[i] = value;
+                entries[i].value = value;
                 return;
             }
         }
-        keys[size] = key;
-        values[size] = value;
+        entries[size] = new Entry<>(key, value);
         size++;
     }
 
     @Override
     public V get(K key) {
         for (int i = 0; i < size; i++) {
-            boolean keysAreEqual = keys[i] == null ? key == null : keys[i].equals(key);
+            boolean keysAreEqual = entries[i].key == null ? key == null : entries[i].key.equals(key);
             if (keysAreEqual) {
-                return values[i];
+                return entries[i].value;
             }
         }
         return null;
